cb7c43aded5cb62364f7fb37351ce554224e0312
problitz <morzel41@gmail.com>
2020-03-03 15:17:05 -0500

    Fixed confirm/cancel button on timepicker screen

--- RestartScheduler/app/src/main/java/rdm/restartscheduler/Main2Activity.java
+++ RestartScheduler/app/src/main/java/rdm/restartscheduler/Main2Activity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
